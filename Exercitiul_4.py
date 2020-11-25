85c72a4758a1d15e399a83972674fc0f1ab5f1af
AlexandraMihalevschi <68810700+AlexandraMihalevschi@users.noreply.github.com>
2020-11-25 21:34:50 +0200

    Delete Exercitiul_4.py

--- Exercitiul_4.py
+++
@@ -1,7 +0,0 @@
-a=str(input("Dati un nume: "))
-b=str(input("Dati o varsta: "))
-c=str(input("Dati un hobby: "))
-d=str(input("Dati o profesie: "))
-print("Cuvintele date sunt:", a, b, c, d)
-print(f"""Numele acelei persone este {a} si are {b} ani.
-Hobby-ul preferat este {c}, cat timp se afla in functia de {d}""")
